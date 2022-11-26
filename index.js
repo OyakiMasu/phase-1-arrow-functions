ab166655378ce452fa2e875e03a7565a579b2c90
OyakMasu21 <akzaki17@gmail.com>
2022-11-26 15:06:20 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+const divide =(num1=2000, num2=100) => {
+    console.log(divide)
+    return num1/num2;
+}
+
+const square = function(square) {
+    let x = 2 
+    console.log(square)
+    return x**2
+}
+
+const add = function(add) {
+  let a = 3
+  let b = 4
+  return a + b
+}
+
+
